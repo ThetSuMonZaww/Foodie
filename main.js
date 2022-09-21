63faf127abd20bf7e2deb2bfe8bfd0ccfcac2606
ThetSuMonZaww <thetsumonzaw92@gmail.com>
2022-09-21 13:25:34 +0630

    third

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
 import './style.scss'
+import Typed from "typed.js";
+
+var typed = new Typed('.element', {
+    strings: ["Food ...", "Drink ..."],
+    typeSpeed: 30,
+    loop: true,
+    backSpeed: 30,
+    backDelay: 3000
+});
